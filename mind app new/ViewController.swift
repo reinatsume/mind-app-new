d1fcdd0500f97ece99a967b4a3256be788ed97ac
夏目怜奈 <reinanna7@icloud.com>
2015-06-22 22:45:39 +0900

    first commit

--- mind app new/ViewController.swift
+++ mind app new/ViewController.swift
@@ -8,13 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var myTextField: UITextField!
+    
+    
+    @IBOutlet var myUIPicker: UIPickerView!
+    var picker1Array: NSArray = ["覚える","ひらめき","調べる","便利"]
+    var picker2Array: NSArray = ["まあまあ","普通","重要"]
+    
+    var mindArray:[String] = []
+
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad()
+        
+    myTextField.text = "hello!"
+    myTextField.delegate = self
+    self.view.addSubview(myTextField)
+    
+    myUIPicker.delegate = self
+    myUIPicker.dataSource = self
+    self.view.addSubview(myUIPicker)
+    
     }
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(component == 0){
+        return picker1Array.count
+        }else if(component == 1){
+            return picker2Array.count
+        }
+        return 0;
+        }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        
+        if(component == 0){
+        return picker1Array[row] as! String
+        }else if (component == 1){
+         return picker2Array[row] as! String
+    }
+        return"";
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if(component == 0){
+            println("列:\(row)")
+            println("値:\(picker1Array[row])")
+        }else if(component == 1){
+            println("列:\(row)")
+            println("値:\(picker2Array[row])")
+        }
+    }
+    
+    @IBAction func didTapmindButton(){
+        self.mindArray.append(self.myTextField.text)
+        performSegueWithIdentifier("to2", sender: nil)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "to2"){
+            var viewController2: ViewController2 = segue.destinationViewController as!ViewController2
+            viewController2.recieveArray = self.mindArray
+            
+            
+        }
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ mind app new/ViewController2.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController2.swift
+//  mind app new
+//
+//  Created by 夏目怜奈 on 2015/06/22.
+//  Copyright (c) 2015年 夏目怜奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    var recieveArray:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+
+}
+
